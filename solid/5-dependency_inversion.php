718cc929bc9ba1f115b164fb90ba0d757cc8a957
Rana Hamdy <104355652+ranahamdy1@users.noreply.github.com>
2025-11-12 23:08:25 +0200

    feat: add Dependency Inversion Principle - DIP

--- solid/5-dependency_inversion.php
+++ solid/5-dependency_inversion.php
@@ -1,1 +1,58 @@
 <?php
+abstract class Calc
+{
+    abstract public function calculate();
+}
+class GetArea extends Calc
+{
+    private $length;
+    private $width;
+    public function __construct($length, $width)
+    {
+        $this->length = $length;
+        $this->width = $width;
+    }
+
+    public function calculate()
+    {
+        return $this->length * $this->width;
+    }
+}
+
+class GetPerimeter extends Calc
+{
+    private $length;
+    private $width;
+    public function __construct($length, $width)
+    {
+        $this->length = $length;
+        $this->width = $width;
+    }
+    public function calculate()
+    {
+        return 2 * ($this->length + $this->width);
+    }
+}
+
+// Shape يعتمد على abstraction (Calc) وليس على concrete class مباشرة
+class Shape
+{
+    private $calc;
+    public function __construct(Calc $calc)
+    {
+        $this->calc = $calc;
+    }
+
+    public function getShapeValue()
+    {
+        return $this->calc->calculate();
+    }
+}
+
+$getArea = new GetArea(5, 3);
+$getAreaShape = new Shape($getArea);
+echo "Area: " . $getAreaShape->getShapeValue() . "\n";
+
+$getPerimeter = new GetPerimeter(5, 3);
+$getPerimeterShape = new Shape($getPerimeter);
+echo "Perimeter: " . $getPerimeterShape->getShapeValue() . "\n";
